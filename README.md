85b350e51f118c444ae6810c7a00a09fe76b401d
Dmitry Plastinin <uncellon@yandex.ru>
2021-08-05 21:43:16 +0300

    README changes

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # Homebrew Libraries Kit - Timers
 
-***
+## License
 
-## To Do:
-1. Readme
-2. Tests
+* * *
+
+<img align="right" src="https://www.gnu.org/graphics/lgplv3-with-text-154x68.png">
+
+The library is licensed under [GNU Lesser General Public License 3.0](https://www.gnu.org/licenses/lgpl-3.0.txt):
+
+Copyright © 2021 Dmitry Plastinin
+
+Hlk Timers is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as pubblished by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+Hlk Timers is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details
--- tests/recalloneshottimer.cpp
+++ tests/recalloneshottimer.cpp
@@ -1,25 +1,3 @@
-/******************************************************************************
- * 
- * Copyright (C) 2021 Dmitry Plastinin
- * Contact: uncellon@yandex.ru, uncellon@gmail.com, uncellon@mail.ru
- * 
- * This file is part of the Hlk Timers library.
- * 
- * Hlk Timers is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as pubblished by the
- * Free Software Foundation, either version 3 of the License, or (at your 
- * option) any later version.
- * 
- * Hlk Timers is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser Public License for more
- * details
- * 
- * You should have received a copy of the GNU Lesset General Public License
- * along with Hlk Timers. If not, see <https://www.gnu.org/licenses/>.
- * 
- *****************************************************************************/
-
 #include "hlk/timers/timer.h"
 
 #include <iostream>
